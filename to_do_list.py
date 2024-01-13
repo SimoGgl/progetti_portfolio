7d66f0311f291798af87186fa1884880ad3f5a53
simone <simone.giglioli@ikmail.com>
2024-01-13 15:13:11 +0100

    Formattazione Output

--- to_do_list.py
+++ to_do_list.py
@@ -3,11 +3,18 @@ to_do_list = ["cane"]
 # Funzione per aggiungere attività
 def add_activity(activity, to_do_list):
     to_do_list.append(activity)
-    input(f"Attività {activity} aggiunta alla lista con successo!")
+    input(f"Attività '{activity}' aggiunta alla lista con successo!")
 
-# Funzione visualizzare le attività
-def see_activity():
-    print(f" Ecco l'elenco delle tue attività:\n'{to_do_list}'\n")
+# Funzione per visualizzare le attività
+def see_activity(to_do_list):
+    if not to_do_list:
+        print("La lista è vuota. Non ci sono attività.")
+    else:
+        print("Lista delle attività:")
+        # itera su ogni elemento della lista to_do_list, assegnando a i l'indice (iniziando da 1) e a activity il valore corrispondente
+        for i, activity in enumerate(to_do_list, start=1):
+            # utilizzo le f-string per stampare gli elementi della lista con il loro indice
+            print(f"{i}. {activity}")
 
 # Funzione per elimiare l'attività
 def delet_activity(activity, to_do_list):
@@ -18,30 +25,41 @@ def delet_activity(activity, to_do_list):
         print(f"L'attività '{activity}' non è presente nella lista")
 
 
-# Funzione principale
 on = True
 
+# Funzione principale
 while on:
-    action = input("Menu:\
-                   \n\t1. Aggiungi attività\
-                   \n\t2. Visualizza attività\
-                   \n\t3. Elimina attività\
-                   \n\t4. Esci\n\n"
-                   "cosa vuoi fare: ")
+    print("\nMenu:")
+    print("\t1. Aggiungi attività")
+    print("\t2. Visualizza attività")
+    print("\t3. Elimina attività")
+    print("\t4. Esci")
     
+    action = input("Cosa vuoi fare: ") 
+
     # Converto in intero per la selezione
-    action = int(action) 
+    try:
+        action = int(action) 
+    except ValueError:
+        print("Inserisci un numero valido")
+        continue
 
     if action == 1:
         activity_to_add = input("Inserisci un'attività: ") 
         add_activity(activity_to_add, to_do_list)
 
     elif action == 2:
-        see_activity()
+        see_activity(to_do_list)
 
     elif action == 3:
-        activity_to_delet = input("Quale attività vuoi rimuovere?\n")
-        delet_activity(activity_to_delet, to_do_list)
+        if not to_do_list:
+            print("La lista è vuota. Non ci sono attività da eliminare.")
+        else:
+            activity_to_delet = input("Quale attività vuoi rimuovere? ")
+            delet_activity(activity_to_delet, to_do_list)
 
     elif action == 4:
         on = False
+
+    else:
+        print("Scelta non valida. Riprova.")
